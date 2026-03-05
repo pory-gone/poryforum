b6ed6f319194c2c47be61c243e95ef6d320f326b
pory-gone <83127821+pory-gone@users.noreply.github.com>
2026-03-05 16:30:19 +0100

    fetching info from api

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,8 +1,17 @@
+const post = [
+  { id: 1, title: 'Primo post', content: 'Contenuto del primo post', votes: 42, author: 'Mario' },
+  { id: 2, title: 'Secondo post', content: 'Contenuto del secondo post', votes: 27, author: 'Luigi' },
+  { id: 3, title: 'Terzo post', content: 'Contenuto del terzo post', votes: 15, author: 'Peach' },
+]
 export default function handler(req, res) {
-    const post = [
-    { id: 1, title: 'Primo post', content: 'Contenuto del primo post', votes: 42, author: 'Mario' },
-    { id: 2, title: 'Secondo post', content: 'Contenuto del secondo post', votes: 27, author: 'Luigi' },
-    { id: 3, title: 'Terzo post', content: 'Contenuto del terzo post', votes: 15, author: 'Peach' },
-  ]
-    res.status(200).json( post )
+  const { id } = req.query
+  if (id) {
+    const post = post.find(p => p.id === Number(id))
+    if (!post) {
+      return res.status(404).json({ error: 'Post is missing...' })
+    }
+    res.status(200).json(post)
+  } else {
+    res.status(200).json(post)
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,10 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export async function getServerSideProps() {
-  const post = [
-    { id: 1, title: 'Primo post', content: 'Contenuto del primo post', votes: 42, author: 'Mario' },
-    { id: 2, title: 'Secondo post', content: 'Contenuto del secondo post', votes: 27, author: 'Luigi' },
-    { id: 3, title: 'Terzo post', content: 'Contenuto del terzo post', votes: 15, author: 'Peach' },
-  ]
-
+  const res = await fetch('http://localhost:3000/api/post')
+  const post = await res.json()
   return {
-    props: {
-      post
-    }
+    props: { post }
   }
 }
 
